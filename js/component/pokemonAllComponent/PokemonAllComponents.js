b5bd090ad893b9834f22a8038c13be91195734d5
maicol torres <maicolt401@gmail.com>
2022-04-25 01:57:34 +0200

    changes on lines to pokemonall

--- js/component/pokemonAllComponent/PokemonAllComponents.js
+++ js/component/pokemonAllComponent/PokemonAllComponents.js
@@ -16,26 +16,26 @@ class PokemonAllComponent extends Component {
 
   render() {
     this.element.innerHTML = `
-       <ul class="characters-list list-unstyled">
-           <li class="character col-12 col-sm-4">
-                <div class="card character__card ">
-                        <img
-                          class="character__picture card-img-top"
-                          src="${this.image}"
-                          alt="foto de ${this.name}"
-                          width="150"
-                          height="150"
-                        />
-                      <div class="card-body">
-                          <h2 class="character__name card-title h4">${this.name}</h2>
-                              <ul class="list-unstyled">
-                                <li>Tipo: ${this.type}</li>
-                                <li>Weight: ${this.weight}</li>
-                              </ul>
-                              <button>agregar</button><button>eliminar</button>
+        <ul class="characters-list list-unstyled">
+            <li class="character col-12 col-sm-4">
+                    <div class="card character__card ">
+                            <img
+                              class="character__picture card-img-top"
+                              src="${this.image}"
+                              alt="foto de ${this.name}"
+                              width="150"
+                              height="150"
+                            />
+                          <div class="card-body">
+                              <h2 class="character__name card-title h4">${this.name}</h2>
+                                  <ul class="list-unstyled">
+                                    <li>Tipo: ${this.type}</li>
+                                    <li>Weight: ${this.weight}</li>
+                                  </ul>
+                                  <button>agregar</button><button>eliminar</button>
+                          </div>
                       </div>
-                  </div>
-           </li>
+            </li>
         </ul>     
   `;
   }
